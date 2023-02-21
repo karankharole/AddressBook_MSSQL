4eb68b0534a286d0d1d02aeafe4dea3baf385f75
karankharole <karankharole2000@gmail.com>
2023-02-21 18:56:41 +0530

    edit contact by using their name

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,4 +1,3 @@
-
 create database AddressBook_serviceDB;
 use AddressBook_serviceDB;
 
@@ -21,6 +20,9 @@ insert into AddressBook(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Em
 values('karan','kharole','amt','AMT','Mah',444601,9689696292,'kk@gmail.com'),
 ('om','kumar','nag','nag','mah',444001,6784736677,'OK@gmail.com'),
 ('ruchita','patil','mum','mah','mah',490020,9675874647,'RP@gmail.com');
-select * from AddressBook;
+select * from  AddressBook;
 
+--UC4 edit contact by person's name--
+update AddressBook set PhoneNumber=786868668 where FirstName='karan';
+select * from AddressBook;
 
